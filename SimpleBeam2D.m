c85469a3612e2eacc66cf0d81d3916abfd1ed21c
mgonzalez-94 <83299272+mgonzalez-94@users.noreply.github.com>
2021-05-11 11:44:00 +0200

    Update SimpleBeam2D.m

--- SimpleBeam2D.m
+++ SimpleBeam2D.m
@@ -1,39 +1,45 @@
+function [St,In,Out] = SimpleBeam2D(L,E,g,Iz,z,ndof,fs,amps,tf,addednoise_snr)
 % SimpleBeam2D
-% 
+%
 % Response a simple supported beam in 2D.
-% 
+%
 % INPUTS:
-% 
-% L: length.
-% E: Young's modulus.
-% g: mass per unit length.
-% Iz: inertia around z axis (m4).
-% ndof: number of degrees of freedom.
-% fs: sample frequency (Hz)
-% Amp: amplitude of the inv(M)*F excitation
-% tf: time length (s)
-% 
+%
+% L: length (sacalar) (m).
+% E: Young's modulus (sacalar) (Pa).
+% g: mass per unit length (sacalar) (kg/m).
+% Iz: inertia around z axis (sacalar) (m4).
+% z: damping ratio (scalar or column vector).
+% ndof: number of degrees of freedom (sacalar).
+% fs: sample frequency (sacalar) (Hz).
+% amps: amplitude of the inv(M)*F excitation (sacalar).
+% tf: time length (sacalar) (s).
+% addednoise_snr: scalar or vector with signal to noise ratio for the added
+%       signal (vector, scalar or empty) (dB).
+%
 % OUTPUTS:
-% 
-% data: text file with [t,Out.y] (s) (m/s2)
-% 
+%
+% St: structure data with the structure info.
+% In: structure data with the input info.
+% Out: structure data with the response info.
+% data: text file with [t,Out.y] (s, m/s2).
+%
 % Units: Internaional system.
-% 
+%
 % %%%%%%%%%%%%%%%%%%%
 % %%% Mateo G. H. %%%
 % %%% 2021/05/09  %%%
 % %%%%%%%%%%%%%%%%%%%
-clc, clearvars, close all;
 %% Structure
 %%% -----------------------------------------------------------------------
 % Mechanical properties
-St.L  = 60; % (m)
-St.E  = 25*1e9; % (Pa)
-St.g  = 1529; % (kg/m)
-St.Iz = 6.75; %(m4)
-St.ndof = 6; % number of dof
+St.L  = L; % (m)
+St.E  = E; % (Pa)
+St.g  = g; % (kg/m)
+St.Iz = Iz; %(m4)
+St.ndof = ndof; % number of dof
 St.n  = (1:St.ndof)';
-St.z  = 0.05*ones(St.ndof,1); % Damping ratio
+St.z  = z.*ones(St.ndof,1); % Damping ratio
 St.Z  = diag(St.z); % Damping ratio
 %%% -----------------------------------------------------------------------
 St.x  = linspace(0,St.L,St.ndof+2); % x divisions
@@ -51,31 +57,31 @@ St.Css = [-St.Ph*St.Wn2, -St.Ph*2*St.Z*St.Wn];
 St.Dss = 0;
 St.Sys = ss(St.Ass,St.Bss,St.Css,St.Dss);
 %% Input
-In.fs   = 500;              % (Hz)
-In.amps = 0.01*9.81;        % (m/s2)
+In.fs   = fs;              % (Hz)
+In.amps = amps;        % (m/s2)
 In.dt   = 1/In.fs;          % (s)
-In.tf   = 60*1;            % (s)
+In.tf   = tf;            % (s)
 In.t    = (0:In.dt:In.tf)'; % (s)
-In.nsam = length(In.t);     % Número de muestras
+In.nsam = length(In.t);     % NÃºmero de muestras
 %%% Noise
 rng('default');
 In.F = In.amps*rand(In.nsam,St.ndof)-In.amps/2; % (m/s2)
 In.F([1,end],:) = 0;
 In.ncha = size(In.F,2);
 %%% Frecuencia
-In.L = length(In.F);                       % Longitud de la señal
-In.Y = 2*(fft(In.F)./In.L);                % (Y(f)) transformada rápida de Fourier.
+In.L = length(In.F);                       % Longitud de la seÃ±al
+In.Y = 2*(fft(In.F)./In.L);                % (Y(f)) transformada rÃ¡pida de Fourier.
 In.f = ((0:length(In.Y)/2-1)*In.fs/In.L)'; % (Hz)
 In.Y = In.Y(1:length(In.f),:);             % (|Y(f)|)
 %% Out
 %%% -----------------------------------------------------------------------
-% Respuesta sin ruido, aceleración total en coord. geom.
+% Respuesta sin ruido, aceleraciÃ³n total en coord. geom.
 Out.y = lsim(St.Sys,In.F,In.t);  % (m/s2)
 %%%
 data{1,1} = [In.t,Out.y];
 %%% Add noise to signal ---------------------------------------------------
 rng('default');
-Out.snr = 5:40; % (dB)
+Out.snr = addednoise_snr; % (dB)
 Lsnr = length(Out.snr);
 Out.yn = cell(Lsnr,1);
 for ii = 1:Lsnr
@@ -94,4 +100,5 @@ for ii=1:length(data)
     FileID = fopen(FileName{ii},'w');
     fprintf(FileID,FileFormat,data{ii}');
     fclose(FileID);
+end
 end
